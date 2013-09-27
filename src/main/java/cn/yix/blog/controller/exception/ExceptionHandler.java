908399d66823a5e4c84c5c3fc5062c4dcbec8e14
yixblog <davepotter@163.com>
2013-09-27 17:22:59 +0800

    异常捕获

---
+++ src/main/java/cn/yix/blog/controller/exception/ExceptionHandler.java
@@ -0,0 +1,37 @@
+package cn.yix.blog.controller.exception;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yixian
+ * Date: 13-9-27
+ * Time: 下午4:18
+ */
+public class ExceptionHandler extends AbstractHandlerExceptionResolver {
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Override
+    protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        String requestUri = request.getRequestURI();
+        if (requestUri.endsWith(".action")) {
+            logger.error(ex);
+            ModelAndView modelAndView = new ModelAndView("jsonView");
+            modelAndView.addObject("success", false);
+            modelAndView.addObject("msg", ex.getMessage());
+            return modelAndView;
+        }
+        try {
+            response.getWriter().write("发生异常:" + ex.getMessage());
+            response.getWriter().flush();
+        } catch (IOException ignored) {
+        }
+        return new ModelAndView();
+    }
+}
